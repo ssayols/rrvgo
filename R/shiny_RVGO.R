a509e5182c368098112176d9e9764ba8f85164df
ssayols <sergisayolspuig@gmail.com>
2018-09-27 17:47:36 +0200

    wordcloud plot

--- R/shiny_RVGO.R
+++ R/shiny_RVGO.R
@@ -1,7 +1,7 @@
 #' shiny_RVGO
+#' Launch an interactive web interface.
 #'
-#' Launch an interactive web interface
-#' @param ... other params sent to shiny::runApp()
+#' @param ... other params sent to shiny::runApp().
 shiny_RVGO <- function(...) {
   shiny::runApp(appDir=system.file("shiny_RVGO", package="RVGO"), ...)
 }
