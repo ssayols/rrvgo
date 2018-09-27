64e461e823591df52054929b4033b7e2cdcab375
ssayols <sergisayolspuig@gmail.com>
2018-09-27 18:09:50 +0200

    simplify wordcloud. Allow summarizing only parent terms

--- R/plotlib.R
+++ R/plotlib.R
@@ -7,6 +7,11 @@
 #' @param pal use custom palette. Defaults to ggplot2's default categorical pal.
 #' @details  Distances between points represent the similarity between terms.
 #' Axes are the first 2 components of applying a PCA to the similarity matrix. 
+#' @examples
+#' go_analysis <- read.delim(system.file("extdata/example2.txt", package="RVGO"), head=FALSE)
+#' simMatrix <- calculateSimMatrix(go_analysis$V1, orgdb="org.Hs.eg.db", ont="BP", method="Rel")
+#' reduced_go_analysis <- reduceSimMatrix(simMatrix, scores, threshold=0.7, orgdb="org.Hs.eg.db")
+#' scatterPlot(simMatrix, reduced_go_analysis)
 scatterPlot <- function(simMatrix, reducedTerms, label=FALSE, pal=NULL) {
   
 }
@@ -21,7 +26,7 @@ scatterPlot <- function(simMatrix, reducedTerms, label=FALSE, pal=NULL) {
 #' @details  Distances between points represent the similarity between terms.
 #' Axes are the first 2 components of applying a PCA to the similarity matrix.
 #' @examples
-#' go_analysis <- read.delim(system.file("extdata/example2.txt", package="RVGO"))
+#' go_analysis <- read.delim(system.file("extdata/example2.txt", package="RVGO"), head=FALSE)
 #' simMatrix <- calculateSimMatrix(go_analysis$V1, orgdb="org.Hs.eg.db", ont="BP", method="Rel")
 #' reduced_go_analysis <- reduceSimMatrix(simMatrix, scores, threshold=0.7, orgdb="org.Hs.eg.db")
 #' treemapPlot(simMatrix, reduced_go_analysis)
@@ -32,26 +37,28 @@ treemapPlot <- function(simMatrix, reducedTerms, label=FALSE, pal=NULL) {
 #' wordlcoudPlot
 #' Plot GO reduced terms as a wordcloud.
 #' 
-#' @param reducedTerms a data.frame with the reduced terms from reduceSimMatrix()
-#' @param pal use custom palette. Defaults to "black"
+#' @param reducedTerms a data.frame with the reduced terms from reduceSimMatrix().
+#' @param onlyParents use only parent terms to calculate frequencies.
+#' @param pal use custom palette.
 #' @examples
-#' go_analysis <- read.delim(system.file("extdata/example2.txt", package="RVGO"))
+#' go_analysis <- read.delim(system.file("extdata/example2.txt", package="RVGO"), head=FALSE)
 #' scores <- setNames(-log10(go_analysis$V2), go_analysis$V1)
 #' simMatrix <- calculateSimMatrix(go_analysis$V1, orgdb="org.Hs.eg.db", ont="BP", method="Rel")
 #' reduced_go_analysis <- reduceSimMatrix(simMatrix, scores, threshold=0.7, orgdb="org.Hs.eg.db")
 #' wordcloudPlot(reduced_go_analysis)
-wordcloudPlot <- function(reducedTerms, pal="black") {
+wordcloudPlot <- function(reducedTerms, onlyParents=TRUE, pal="black") {
   if(!all(sapply(c("wordcloud", "tm", "slam"), requireNamespace, quietly=TRUE))) {
     stop("Package wordcloud and/or its dependencies (tm, slam) not available. ",
          "Consider installing it before using this function.", call.=FALSE)
   }
     
-  x <- tm::Corpus(tm::VectorSource(reducedTerms$term))
-  x <- tm::tm_map(x, tm::PlainTextDocument)
-  x <- tm::tm_map(x, tm::stripWhitespace)
-  x <- tm::tm_map(x, tm::removePunctuation)
-  x <- tm::tm_map(x, function(x) tm::removeWords(x, tm::stopwords()))
-  tdm <- tm::TermDocumentMatrix(x)
+  if(onlyParents) {
+    x <- tm::Corpus(tm::VectorSource(reducedTerms$term[reducedTerms$parent == ""]))
+  } else {
+    x <- tm::Corpus(tm::VectorSource(reducedTerms$term))
+  }
+  tdm <- tm::TermDocumentMatrix(x, control=list(removePunctuation=TRUE,
+                                                stopwords=TRUE))
   m <- as.matrix(tdm)
   v <- sort(rowSums(m), decreasing=TRUE)
   d <- data.frame(word=names(v), freq=v)
