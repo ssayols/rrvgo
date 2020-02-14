24c011badf25ba9c338fb7d071df82f145677791
ssayols <sergisayolspuig@gmail.com>
2020-02-14 17:49:10 +0100

    add example data

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' prepareDemoData
+#' 
+#' Prepare demo data included in the package.
+#' 
+#' @details Taken asis from https://yulab-smu.github.io/clusterProfiler-book/chapter3.html#input-data,
+#' which at the same time it is taken from the R package breastCancerMAINZ. It
+#' contains 200 samples with breast cancer at different grades (I, II and III).
+#' The dataset basically contains log2 ratios of the geometric means of
+#' grade III vs. grade I samples.
+prepareDemoData <- function() {
+  data(geneList, package="DOSE")
+  gene <- names(geneList)[abs(geneList) > 2]
+  orgdb <- rrvgo:::loadOrgdb("org.Hs.eg.db")
+  ego <- clusterProfiler::enrichGO(gene          = gene,
+                                   universe      = names(geneList),
+                                   OrgDb         = orgdb,
+                                   ont           = "BP",
+                                   pAdjustMethod = "BH",
+                                   pvalueCutoff  = 0.01,
+                                   qvalueCutoff  = 0.05,
+                                   readable      = TRUE)
+  write.table(subset(ego@result, qvalue < .05), file="inst/extdata/example.txt", sep="\t")
+}
